08d32ba66e708fed4ef922db4702195ea96422e4
GSM Rana <gsmrana@gmail.com>
2019-09-12 21:44:28 +0600

    Initial design

---
+++ File Generator/MainForm.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace File_Generator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.textBoxFileName = new System.Windows.Forms.TextBox();
+            this.textBoxFileContentTemp = new System.Windows.Forms.TextBox();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.comboBoxFileSize = new System.Windows.Forms.ComboBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.comboBoxFileSizeUnit = new System.Windows.Forms.ComboBox();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxFileName
+            // 
+            this.textBoxFileName.Location = new System.Drawing.Point(12, 98);
+            this.textBoxFileName.Name = "textBoxFileName";
+            this.textBoxFileName.Size = new System.Drawing.Size(120, 20);
+            this.textBoxFileName.TabIndex = 3;
+            this.textBoxFileName.Text = "G:\\Test1.txt";
+            // 
+            // textBoxFileContentTemp
+            // 
+            this.textBoxFileContentTemp.Location = new System.Drawing.Point(12, 12);
+            this.textBoxFileContentTemp.Multiline = true;
+            this.textBoxFileContentTemp.Name = "textBoxFileContentTemp";
+            this.textBoxFileContentTemp.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxFileContentTemp.Size = new System.Drawing.Size(280, 80);
+            this.textBoxFileContentTemp.TabIndex = 2;
+            this.textBoxFileContentTemp.Text = "The quick brown fox jumps over the lazy dog!\r\n";
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Location = new System.Drawing.Point(227, 98);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(65, 47);
+            this.buttonCreate.TabIndex = 0;
+            this.buttonCreate.Text = "Create";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.ButtonCreate_Click);
+            // 
+            // comboBoxFileSize
+            // 
+            this.comboBoxFileSize.FormattingEnabled = true;
+            this.comboBoxFileSize.Items.AddRange(new object[] {
+            "1",
+            "8",
+            "16",
+            "32",
+            "64",
+            "128",
+            "256",
+            "512",
+            "1024"});
+            this.comboBoxFileSize.Location = new System.Drawing.Point(12, 124);
+            this.comboBoxFileSize.Name = "comboBoxFileSize";
+            this.comboBoxFileSize.Size = new System.Drawing.Size(120, 21);
+            this.comboBoxFileSize.TabIndex = 4;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 151);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(280, 14);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 1;
+            // 
+            // comboBoxFileSizeUnit
+            // 
+            this.comboBoxFileSizeUnit.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxFileSizeUnit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.comboBoxFileSizeUnit.FormattingEnabled = true;
+            this.comboBoxFileSizeUnit.Items.AddRange(new object[] {
+            "Bytes",
+            "KBytes",
+            "MBytes"});
+            this.comboBoxFileSizeUnit.Location = new System.Drawing.Point(141, 124);
+            this.comboBoxFileSizeUnit.Name = "comboBoxFileSizeUnit";
+            this.comboBoxFileSizeUnit.Size = new System.Drawing.Size(80, 21);
+            this.comboBoxFileSizeUnit.TabIndex = 5;
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Location = new System.Drawing.Point(141, 96);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(80, 23);
+            this.buttonBrowse.TabIndex = 6;
+            this.buttonBrowse.Text = "Browse";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.ButtonBrowse_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.Location = new System.Drawing.Point(12, 168);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(280, 13);
+            this.labelStatus.TabIndex = 7;
+            this.labelStatus.Text = "labelStatus";
+            this.labelStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 185);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.comboBoxFileSizeUnit);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.comboBoxFileSize);
+            this.Controls.Add(this.buttonCreate);
+            this.Controls.Add(this.textBoxFileContentTemp);
+            this.Controls.Add(this.textBoxFileName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "File Generator";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxFileName;
+        private System.Windows.Forms.TextBox textBoxFileContentTemp;
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.ComboBox comboBoxFileSize;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ComboBox comboBoxFileSizeUnit;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.Label labelStatus;
+    }
+}
+
---
+++ File Generator/MainForm.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace File_Generator
+{
+    public partial class MainForm : Form
+    {
+        #region Const and Data
+
+        const int RW_BLOCK_SIZE = 512;
+        readonly string _defaultFileName = "Test1.txt";
+
+        bool _creatingfile = false;
+        int _targetsize;
+        string _targetFileName;
+        string _targetTemplate;
+
+        #endregion
+
+        #region ctor
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Text = ProductName + " v" + Assembly.GetExecutingAssembly().GetName().Version;
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            try
+            {
+                comboBoxFileSize.SelectedIndex = 0;
+                comboBoxFileSizeUnit.SelectedIndex = 1;
+                textBoxFileName.Text = _defaultFileName;
+                var drive = DriveInfo.GetDrives().Last(d => d.DriveType <= DriveType.Fixed);
+                textBoxFileName.Text = Path.Combine(drive.Name, _defaultFileName);
+                labelStatus.Text = "Ready";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        #endregion
+
+        #region Internal Method
+
+
+        void UpdateStatus(int percent)
+        {
+            var status = string.Format("Writing {0}%", percent);
+            Invoke(new Action(() =>
+            {
+                progressBar1.Value = percent;
+                labelStatus.Text = status;
+            }));
+        }
+
+        static IEnumerable<string> SplitToBlocks(string str, int chunkSize)
+        {
+            return Enumerable.Range(0, str.Length / chunkSize).Select(i => str.Substring(i * chunkSize, chunkSize));
+        }
+
+        void CreateFileHandler(string filename, string template, int targetfilesize)
+        {
+            var contentstr = new StringBuilder(targetfilesize);
+            var appendcount = targetfilesize / template.Length;
+            var remainigsize = targetfilesize % template.Length;
+            for (int i = 0; i < appendcount; i++) contentstr.Append(template);
+            if (remainigsize != 0) contentstr.Append(template.Substring(0, remainigsize));
+
+            var prevpercent = 0;
+            var blocksWritten = 0;
+            var blocks = SplitToBlocks(contentstr.ToString(), RW_BLOCK_SIZE).ToList();
+            using (var sw = new StreamWriter(filename))
+            {
+                foreach (var item in blocks)
+                {
+                    sw.Write(item);
+                    var percent = (blocksWritten * 100) / blocks.Count;
+                    if (percent > prevpercent)
+                    {
+                        UpdateStatus(percent);
+                        prevpercent = percent;
+                    }
+                    blocksWritten++;
+                }
+                var remainigbytes = targetfilesize % RW_BLOCK_SIZE;
+                if (remainigbytes != 0)
+                {
+                    var remainigstr = contentstr.ToString().Substring(blocksWritten * RW_BLOCK_SIZE, remainigbytes);
+                    sw.Write(remainigstr);
+                }
+                UpdateStatus(100);
+            }
+        }
+
+        void RunCreateFileThread()
+        {
+            Task.Factory.StartNew(new Action(() =>
+            {
+                _creatingfile = true;
+                try
+                {
+                    CreateFileHandler(_targetFileName, _targetTemplate, _targetsize);
+                    Invoke(new Action(() =>
+                    {
+                        labelStatus.ForeColor = Color.DarkGreen;
+                        labelStatus.Text = "Success";
+                        buttonCreate.Text = "Create";
+                    }));
+                }
+                catch (Exception ex)
+                {
+                    Invoke(new Action(() =>
+                    {
+                        labelStatus.ForeColor = Color.Red;
+                        labelStatus.Text = "Failed";
+                        buttonCreate.Text = "Create";
+                        var msg = string.Format("{0}Destination: {1}", ex.Message, _targetFileName);
+                        MessageBox.Show(msg, "File Creating Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }));
+                }
+                _creatingfile = false;
+            }));
+        }
+
+        #endregion
+
+        #region Form Events
+
+        private void ButtonBrowse_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var sfd = new SaveFileDialog())
+                {
+                    sfd.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                    sfd.FileName = _defaultFileName;
+                    if (sfd.ShowDialog() == DialogResult.OK)
+                    {
+                        textBoxFileName.Text = sfd.FileName;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ButtonCreate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (_creatingfile) return;
+                _targetFileName = textBoxFileName.Text;
+                _targetTemplate = textBoxFileContentTemp.Text;
+                var targetsize = int.Parse(comboBoxFileSize.Text);
+                var filesizeunit = comboBoxFileSizeUnit.SelectedIndex;
+                if (filesizeunit == 1) targetsize *= 1024;
+                else if (filesizeunit == 2) targetsize *= 1024 * 1024;
+                _targetsize = targetsize;
+
+                buttonCreate.Text = "Creating...";
+                progressBar1.Value = 0;
+                labelStatus.ForeColor = Color.Blue;
+                labelStatus.Text = "Writing...";
+                RunCreateFileThread();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        #endregion
+
+    }
+}
